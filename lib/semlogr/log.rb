d975326893b296f4805a3329b7c5a6fb54fd5660
Stefan Sedich <stefan.sedich@gmail.com>
2016-10-11 13:45:33 -0700

    Expose logger instance

--- lib/semlogr/log.rb
+++ lib/semlogr/log.rb
@@ -8,39 +8,43 @@ module Semlogr
       end
     end
 
+    def self.logger
+      @logger
+    end
+
     def self.debug(template = nil, **properties, &block)
       ensure_logger
 
-      @logger.debug(template, **properties, &block)
+      logger.debug(template, **properties, &block)
     end
 
     def self.info(template = nil, **properties, &block)
       ensure_logger
 
-      @logger.info(template, **properties, &block)
+      logger.info(template, **properties, &block)
     end
 
     def self.warn(template = nil, **properties, &block)
       ensure_logger
 
-      @logger.warn(template, **properties, &block)
+      logger.warn(template, **properties, &block)
     end
 
     def self.error(template = nil, **properties, &block)
       ensure_logger
 
-      @logger.error(template, **properties, &block)
+      logger.error(template, **properties, &block)
     end
 
     def self.fatal(template = nil, **properties, &block)
       ensure_logger
 
-      @logger.fatal(template, **properties, &block)
+      logger.fatal(template, **properties, &block)
     end
 
     class << self
       def ensure_logger
-        raise StandardError, 'You need to initialize the logger instance by calling Semlogr::Log.configure first!' unless @logger
+        raise StandardError, 'You need to initialize the logger instance by calling Semlogr::Log.configure first!' unless logger
       end
     end
   end
