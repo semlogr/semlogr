534183f8326ecc21cfe174c7ffcbab04734204c0
Stefan Sedich <stefan.sedich@gmail.com>
2018-11-12 21:38:59 -0800

    cleanup

--- lib/semlogr/formatters/property_value_formatter.rb
+++ lib/semlogr/formatters/property_value_formatter.rb
@@ -5,28 +5,33 @@ module Semlogr
     class PropertyValueFormatter
       def self.format(value, format)
         return '(nil)' unless value
+        return format_using_format_string(value, format) if format
 
-        if format
+        case value
+        when String
+          "\"#{value}\""
+        when StandardError
+          formatted_error = +"#{value.class}: #{value.message}"
+
+          if value.backtrace&.any?
+            formatted_error << "\n\s\s#{value.backtrace.join("\n\s\s")}"
+          end
+
+          formatted_error << "\n"
+        else
+          value.to_s
+        end
+      end
+
+      class << self
+        private
+
+        def format_using_format_string(value, format)
           case value
           when DateTime, Date, Time
             value.strftime(format)
           else
             Kernel.format("%#{format}", value)
-          end
-        else
-          case value
-          when String
-            "\"#{value}\""
-          when StandardError
-            formatted_error = +"#{value.class}: #{value.message}"
-
-            if value.backtrace&.any?
-              formatted_error << "\n\s\s#{value.backtrace.join("\n\s\s")}"
-            end
-
-            formatted_error << "\n"
-          else
-            value.to_s
           end
         end
       end
