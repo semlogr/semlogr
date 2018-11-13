cd5f2e7c4f9aea57428e679fb6805f01e8eb75fc
Stefan Sedich <stefan.sedich@gmail.com>
2018-11-12 21:45:02 -0800

    Extract format_value

--- lib/semlogr/formatters/property_value_formatter.rb
+++ lib/semlogr/formatters/property_value_formatter.rb
@@ -5,33 +5,37 @@ module Semlogr
     class PropertyValueFormatter
       def self.format(value, format)
         return '(nil)' unless value
-        return format_using_format_string(value, format) if format
+        return format_value_with_format_string(value, format) if format
 
-        case value
-        when String
-          "\"#{value}\""
-        when StandardError
-          formatted_error = +"#{value.class}: #{value.message}"
-
-          if value.backtrace&.any?
-            formatted_error << "\n\s\s#{value.backtrace.join("\n\s\s")}"
-          end
-
-          formatted_error << "\n"
-        else
-          value.to_s
-        end
+        format_value(value)
       end
 
       class << self
         private
 
-        def format_using_format_string(value, format)
+        def format_value_with_format_string(value, format)
           case value
           when DateTime, Date, Time
             value.strftime(format)
           else
             Kernel.format("%#{format}", value)
+          end
+        end
+
+        def format_value(value)
+          case value
+          when String
+            "\"#{value}\""
+          when StandardError
+            formatted_error = +"#{value.class}: #{value.message}"
+
+            if value.backtrace&.any?
+              formatted_error << "\n\s\s#{value.backtrace.join("\n\s\s")}"
+            end
+
+            formatted_error << "\n"
+          else
+            value.to_s
           end
         end
       end
